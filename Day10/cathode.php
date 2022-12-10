6e1e1854e5b53673d882f65db90a140490ec3306
Ross Steele <ross.steele@huler.io>
2022-12-10 19:19:08 +0000

    day10 p2

--- Day10/cathode.php
+++ Day10/cathode.php
@@ -8,14 +8,23 @@ class Cathode
     protected bool $testing = false;
     public int $signalStrength = 0;
 
+    public array $grid = [];
 
     public function __construct()
     {
         $this->items = array_filter(explode(PHP_EOL, file_get_contents($this->testing ? './test_input.txt' : './input.txt')),
             fn($item) => $item !== '');
+
+        foreach(range(0, 5) as $row) {
+            $row = [];
+            foreach(range(0, 39) as $column) {
+                $row[$column] = ".";
+            }
+            $this->grid[]  = $row;
+        }
     }
 
-    public function solve(): int
+    public function solve1(): int
     {
         foreach ($this->items as $item) {
             if (str_starts_with($item, "noop")) {
@@ -31,14 +40,46 @@ class Cathode
         return $this->signalStrength;
     }
 
-    private function cycle(): void
+    public function solve2(): void
+    {
+        foreach ($this->items as $item) {
+            if (str_starts_with($item, "noop")) {
+                $this->cycle(true);
+            } else {
+                $operations = explode(" ", $item);
+                $this->cycle(true);
+                $this->cycle(true);
+                $this->register += (int)$operations[1];
+            }
+        }
+
+
+        foreach(range(0, 5) as $row) {
+            foreach(range(0, 39) as $column) {
+                print $this->grid[$row][$column];
+            }
+            print PHP_EOL;
+        }
+    }
+
+    private function cycle(bool $turnOnLed = false): void
     {
         if (in_array($this->cycle, [20, 60, 100, 140, 180, 220])) {
             $this->signalStrength += $this->cycle * $this->register;
+        }
+
+        if($turnOnLed) {
+            if(abs((($this->cycle % 40) - 1) - $this->register) <= 1) {
+                $this->grid[floor($this->cycle / 40)][($this->cycle % 40) - 1] = "#";
+            }
         }
 
         $this->cycle += 1;
     }
 }
 
-print "Part 1 : " . (new Cathode())->solve() .PHP_EOL;
+print "Part 1 : " . (new Cathode())->solve1() .PHP_EOL;
+
+print "Part 2 : " . PHP_EOL;
+(new Cathode())->solve2();
+
